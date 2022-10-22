64e5026ea6a98d53cb54cc7ae291540ca5360071
robinson <robinpaulista85@gmail.com>
2022-10-22 12:31:43 -0300

    If and Else/Python

--- exercicio1.py
+++ exercicio1.py
@@ -1,1 +1,9 @@
 # 1. Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+char=input('Digite um caractere: ')
+
+if char=='a' or char=='e' or char=='i' or char=='o' or char=='u' or \
+    char=='A' or char=='E' or char=='I' or char=='O' or char=='U':
+        print('Vogal')
+else:
+    print('Consoante')
